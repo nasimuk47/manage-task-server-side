66659b03137bd6c3208a8d241c9a23a3752e36f9
nasimuk47 <nasim.uk47@gmail.com>
2023-12-22 10:32:46 +0600

    get user and all task data

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const { ObjectId } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtgfrql.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const userCollection = client.db("TaskManage").collection("users");
+        const AllTaskCollection = client.db("TaskManage").collection("AllTask");
+
+        // --------------------------------------------------------------------
+
+        // user related
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({
+                    message: "user already exists",
+                    insertedId: null,
+                });
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        // task history related
+        app.post("/AllTask", async (req, res) => {
+            const task = req.body;
+            const result = await AllTaskCollection.insertOne(task);
+
+            if (result.insertedId) {
+                res.send({
+                    message: "Task added successfully",
+                    taskId: result.insertedId,
+                });
+            } else {
+                res.status(500).send({
+                    message: "Failed to add task",
+                    taskId: null,
+                });
+            }
+        });
+
+        // Get all tasks by user email
+        app.get("/AllTask", async (req, res) => {
+            const result = await AllTaskCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.delete("/AllTask/:taskId", async (req, res) => {
+            const taskId = req.params.taskId; // Fix this line
+            const query = { _id: new ObjectId(taskId) };
+            const result = await AllTaskCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // ---------------------------------------
+
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("TaskManger ready.......................");
+});
+
+app.listen(port, () => {
+    console.log(`task Ready on port ${port}`);
+});
